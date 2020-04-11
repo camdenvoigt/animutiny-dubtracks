a3485be56c9fcc3b841dab054ede923d20ef9527
= <cjvoigt@iastate.edu>
2020-04-11 13:46:32 -0500

    Added Signup Dialog

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,27 +1,53 @@
 <template>
-  <div id="app">
-    <YoutubePlayer video_url='https://www.youtube.com/embed/videoseries?list=PLx0sYbCqOb8TBPRdmBHs5Iftvv9TPboYG'/> 
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <h3>Animutiny Dubtracks</h3>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        target="_blank"
+        text
+        @click.stop="signupDialog = true"
+      >
+        <span class="mr-2">Sign Up</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <YoutubePlayer video_url='https://www.youtube.com/embed/5wvTs-1jUFE'/>
+    </v-content>
+
+    <SignupPopover :showDialog="signupDialog" v-on:close-signup-dialog="closeSignupModal"/>
+  </v-app>
 </template>
 
 <script>
-import YoutubePlayer from './components/YoutubePlayer.vue'
+import YoutubePlayer from './components/YoutubePlayer'
+import SignupPopover from './components/SignupPopover'
 
 export default {
   name: 'App',
-  components: {
-    YoutubePlayer
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    YoutubePlayer,
+    SignupPopover
+  },
+
+  data: () => ({
+    signupDialog: false
+  }),
+
+  methods: {
+    closeSignupModal () {
+      this.signupDialog = false;
+    }
+  }
+};
+</script>
---
+++ client/src/components/SignupPopover.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-dialog v-model="showDialog" max-width="500" @click:outside="$emit('close-signup-dialog')">
+    <v-card>
+          <v-card-title>
+            <span class="headline">Sign Up</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field 
+                    v-model="username"
+                    label="Username*" 
+                    :rules="usernameRules"
+                    :counter="16"
+                    required></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field 
+                    v-model="password"
+                    label="Password*" 
+                    :rules="passwordRules"
+                    :counter="10"
+                    type="password"
+                    required></v-text-field>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="$emit('close-signup-dialog')">Close</v-btn>
+            <v-btn color="blue darken-1" text @click="submit">Save</v-btn>
+          </v-card-actions>
+        </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props:['showDialog'],
+  data:() => ({
+    valid: true,
+    username: '',
+    password: '',
+    usernameRules: [
+      v => !!v || 'Name is required',
+      v => (v && v.length <= 16) || 'Name must be less than 10 characters',
+    ],
+    passwordRules: [
+      v => !!v || 'Password is required',
+      v => (v && v.length >= 10) || 'Name must be more than 10 characters'
+    ]
+  }),
+  methods: {
+    submit() {
+      //TODO: Submit data and exit modal
+      this.$emit('close-signup-dialog')
+    }
+  }
+}
+</script>
