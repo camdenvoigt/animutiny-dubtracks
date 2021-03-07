7c8aea0c46c7671d785f7f9a3b994edb20862180
= <cjvoigt@iastate.edu>
2021-03-07 10:23:36 -0600

    place video player

--- client/src/components/YoutubePlayer.vue
+++ client/src/components/YoutubePlayer.vue
@@ -1,7 +1,10 @@
 <template>
-  <iframe width="560" height="315" 
-  :src="video_url"
-  frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+  <div class="video-player-container">
+    <iframe class="video-iframe" 
+      width="670" height="380" 
+      :src="video_url"
+      frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+  </div>
 </template>
 
 <script>
@@ -13,5 +16,9 @@ export default {
 </script>
 
 <style scoped>
-
+.video-player-container {
+  position: absolute;
+  top: 15%;
+  left: 5%;
+}
 </style>
